bc04929d37fa6c03f54c1ffb05a1f57ad8702ec5
emV8 <emeline.vannet@laposte.net>
2016-04-16 18:28:58 +0200

    Aide

--- form/form10.php
+++ form/form10.php
@@ -36,7 +36,30 @@
 		       <input type="checkbox" name="namemoodplaylist" value="fatigue" /> Fatigue <br/>
 		       <input type="checkbox" name="namemoodplaylist" value="ennui" /> Ennui <br/>
 		       <input type="checkbox" name="namemoodplaylist" value="tristesse" /> Tristesse <br/>
-		       <input type="checkbox" name="namemoodplaylist" value="autre" /> Autre : <input type = "text" name = "othermoodplaylist" id = "autre" placeholder = "Autre humeur" style="text-align:center"/> <br/>
+		       <input type="checkbox" name="namemoodplaylist" value="autre" /> Autre : 
+		       <a href="#" class="test">[?]
+						<div class="raised">
+						<b class="top"><b class="b1"></b><b class="b2"></b><b class="b3"></b><b class="b4"></b></b>
+						<div class="boxcontent">
+						 
+						<b> Exemple d'autres adjectifs possibles pour qualifier votre humeur : </b>
+						<p>
+						 
+						Tranquille - À l'aise - Attentif - Concentré ... <br />
+						Surpris - Choqué - Confus - Dérouté ... <br />
+						Abbatu - Chagriné - Démoralisé - Déprimé ... <br />
+						Acerbe - Agacé - Amer - Contrarié ... <br />
+						Furieux - Enragé - Furax - Furibon ... <br />
+						Affolé - Alarmé - Angoissé - Anxieux ... <br />
+						Dégoûté - Écœuré - Rébuté - Révulsé ... <br />
+						Amusé - Comblé - Content - Égayé ... <br />
+						... </br>
+						</p>
+						</div>
+						<b class="bottom"><b class="b4b"></b><b class="b3b"></b><b class="b2b"></b><b class="b1b"></b></b>
+						</div>
+					</a>
+		       <input type = "text" name = "othermoodplaylist" id = "autre" placeholder = "Autre humeur" style="text-align:center"/> <br/>
 			<input class = "suivant" type="submit" value="Suivant >" />
 			<br/>
 			<progress max="10" value = "10"></progress>
