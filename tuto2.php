b422421d4f9da20b9e26e1a8d994d91b5b68a9bc
emV8 <emeline.vannet@laposte.net>
2016-04-21 13:43:51 +0200

    11 questions et pas 10

--- tuto2.php
+++ tuto2.php
@@ -33,7 +33,7 @@
         <div id = "tuto2">
         	Vous écoutez la playlist n° <?php echo $_SESSION['playlist_id'] ?>. <br/>
             À la fin de cette playlist, vous passerez au questionnaire. <br/>
-            Ce dernier est composé de 10 questions et chacune est obligatoire. <br/>
+            Ce dernier est composé de 11 questions et chacune est obligatoire. <br/>
             N'hésitez pas à remplir le champ <i> "autre" </i> si aucune des formulations ne vous convient. <br/>
             Merci de répondre le plus sincèrement possible à ces questions et de quitter la page, si vous le souhaitez, après avoir répondu à toutes les questions. <br/> <br/>
             Bon courage pour le questionnaire !
