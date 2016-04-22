a429995463a159ca12ac2c65ecf2d6fee2bdd724
Somnir <matthieu.burtin.somnir@gmail.com>
2016-04-22 09:53:41 +0200

    commit imposé

--- form/form10.php
+++ form/form10.php
@@ -38,10 +38,9 @@
 			<input class = "suivant" type="submit" value="Suivant >" />
 		       <input type="checkbox" name="namemoodplaylist" value="autre" /> Autre : <input type = "text" name = "othermoodplaylist" id = "autre" placeholder = "Autre humeur" style="text-align:center"/> <br/>
 			<input id = "last" class = "suivant" type="submit" value="Suivant" onclick="loadForm(this)">
-<<<<<<< HEAD
+
 			<!--<input class = "suivant" type="submit" value="Suivant" onclick="self.location.href='tuto2.php'">-->
-=======
->>>>>>> origin/master
+
 			<br/>
 			<progress max="10" value = "10"></progress>
 		<br/>
